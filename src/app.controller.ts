8f7977bc3270753b90d034d1e34c2c611452cab6
BayuDC <dev.bayudc@gmail.com>
2023-02-14 18:01:29 +0700

    feat: calculate time for countdown

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, GoneException, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import moment from 'moment';
 
 @Controller()
 export class AppController {
@@ -9,5 +10,25 @@ export class AppController {
   @Render('index.hbs')
   index() {
     return { message: 'Hello world!' };
+  }
+
+  @Get('/info')
+  info() {
+    const now = moment(new Date());
+    const end = moment(this.appService.getConfig().date);
+    const diff = moment.duration(end.diff(now));
+
+    if (diff.seconds() < 0) {
+      throw new GoneException();
+    }
+
+    return {
+      time: {
+        day: diff.days(),
+        hour: diff.hours(),
+        minute: diff.minutes(),
+        second: diff.seconds(),
+      },
+    };
   }
 }
